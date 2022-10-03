b4e4f75d448bdb392d3ebaf958ed9ff9b68f7570
kiku99 <elsd0326@gmail.com>
2022-10-03 21:30:10 +0900

    first init

---
+++ src/main/java/com/example/board/domain/post/PostRequest.java
@@ -0,0 +1,15 @@
+package com.example.board.domain.post;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostRequest {
+    private Long id;             // PK
+    private String title;        // 제목
+    private String content;      // 내용
+    private String writer;       // 작성자
+    private Boolean noticeYn;    // 공지글 여부
+}
