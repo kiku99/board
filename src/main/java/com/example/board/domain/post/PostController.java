1d5dcb0236f9924b78436c3c9646f7bf4e107ec3
kiku99 <elsd0326@gmail.com>
2022-10-04 20:50:09 +0900

    글쓰기 작성 페이지 생성

--- src/main/java/com/example/board/domain/post/PostController.java
+++ src/main/java/com/example/board/domain/post/PostController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,14 +15,18 @@ public class PostController {
 
     //게시글 작성 페이지
     @GetMapping("/post/write.do")
-    public String openPostWrite(Model model){
-        String title = "제목",
-        content = "내용",
-        writer = "홍길동";
-
-        model.addAttribute("t", title);
-        model.addAttribute("c", content);
-        model.addAttribute("w", writer);
+    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model){
+        if (id != null){
+            PostResponse post = postService.findPostById(id);
+            model.addAttribute("post", post);
+        }
         return "/post/write";
+    }
+
+    //신규 게시글 생성
+    @PostMapping("/post/save.do")
+    public String savePost(final PostRequest params){
+        postService.savePost(params);
+        return "redirect:/post/list.do";
     }
 }
