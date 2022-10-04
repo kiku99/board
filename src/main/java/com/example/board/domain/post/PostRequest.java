8fc9497dff3eb837032edfdbba224224f1689bb0
kiku99 <elsd0326@gmail.com>
2022-10-04 12:19:36 +0900

    SQL mapping 및 테스트코드 작성

--- src/main/java/com/example/board/domain/post/PostRequest.java
+++ src/main/java/com/example/board/domain/post/PostRequest.java
@@ -8,8 +8,8 @@ import lombok.Setter;
 @Setter
 public class PostRequest {
     private Long id;             // PK
-    private String title;        // 제목
-    private String content;      // 내용
-    private String writer;       // 작성자
-    private Boolean noticeYn;    // 공지글 여부
+    private String title;        //
+    private String content;      //
+    private String writer;       //
+    private Boolean noticeYn;    //
 }
--- src/main/java/com/example/board/domain/post/PostResponse.java
+++ src/main/java/com/example/board/domain/post/PostResponse.java
@@ -8,13 +8,13 @@ import java.time.LocalDateTime;
 public class PostResponse {
 
     private Long id;                       // PK
-    private String title;                  // 제목
-    private String content;                // 내용
-    private String writer;                 // 작성자
-    private int viewCnt;                   // 조회 수
-    private Boolean noticeYn;              // 공지글 여부
-    private Boolean deleteYn;              // 삭제 여부
-    private LocalDateTime createdDate;     // 생성일시
-    private LocalDateTime modifiedDate;    // 최종 수정일시
+    private String title;                  //
+    private String content;                //
+    private String writer;                 //
+    private int viewCnt;                   //
+    private Boolean noticeYn;              //
+    private Boolean deleteYn;              //
+    private LocalDateTime createdDate;     //
+    private LocalDateTime modifiedDate;    //
 
 }
